8dfbdd866255989596107919743eba1132bff1a8
Cameron McGifford <cameronmcgifford96@gmail.com>
2025-07-13 14:07:53 +0100

    token controller done and remapped OAS imports to target folder for regen

--- eagle/src/main/java/com/takehome/eagle/controller/TokenController.java
+++ eagle/src/main/java/com/takehome/eagle/controller/TokenController.java
@@ -1,40 +1,44 @@
-//package com.takehome.eagle.controller;
-//
-//import io.jsonwebtoken.Jwts;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.openapitools.client.model.UserResponse;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Date;
-//
-//@RestController
-//@Slf4j
-//@RequestMapping("/v1/auth")
-//@RequiredArgsConstructor
-//public class TokenController extends AuthApi{
-//
-//    @Value("${jwt.secret}")
-//    private String secretKey;
-//
-//    @PostMapping
-//    public String getToken(@RequestBody UserResponse userResponse) {
-//        return generateJwtToken(userResponse.getId());
-//    }
-//
-//
-//    public String generateJwtToken(String userId) {
-//        // Assuming you have a secret key for signing the JWT
-//        String jwt = Jwts.builder()
-//                .setSubject(userId)
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour
-////                .signWith(secretKey)
-//                .compact();
-//        return jwt;
-//    }
-//}
+package com.takehome.eagle.controller;
+
+import com.takehome.eagle.api.AuthApi;
+import com.takehome.eagle.model.UserResponse;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@RestController
+@Slf4j
+@RequestMapping("/v1/auth")
+@RequiredArgsConstructor
+public class TokenController implements AuthApi {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @PostMapping
+    public String getToken(@RequestBody UserResponse userResponse) {
+        return generateJwtToken(userResponse.getId());
+    }
+
+
+    public String generateJwtToken(String userId) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
--- eagle/src/main/java/com/takehome/eagle/controller/UserController.java
+++ eagle/src/main/java/com/takehome/eagle/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.takehome.eagle.controller;
 
 import com.takehome.eagle.service.UserService;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.openapitools.client.api.UserApi;
-import org.openapitools.client.model.UserResponse;
 import org.springframework.web.bind.annotation.*;
+import com.takehome.eagle.api.UserApi;
 
 @RestController
 @Slf4j
